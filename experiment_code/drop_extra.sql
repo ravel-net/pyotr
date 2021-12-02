e0392d70c370e1387dc941ed6d3992602ac0af1f
mudbri <mubashiranwar@live.com>
2021-12-02 01:28:23 -0600

    Scripts for experiment cleanup

---
+++ experiment_code/drop_extra.sql
@@ -0,0 +1,15 @@
+drop table rib1000_r;
+drop table rib1000_r1;
+drop table rib1000_r2;
+drop table rib1000_r3;
+drop table rib1000_r4;
+drop table rib1000_r5;
+drop table rib1000_r6;
+drop table rib1000_r7;
+drop table rib1000_r8;
+drop table rib1000_r9;
+drop table rib1000_r10;
+drop table rib1000;
+drop table rib10000;
+drop table rib0610;
+drop table f_table_rib1000;
