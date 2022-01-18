57b74d97aa618a78d9de3fb7c30eac22ae59e268
mudbri <mubashiranwar@live.com>
2022-01-18 17:55:26 +0500

    Minimization bug fixes

--- util/minimization_faure.py
+++ util/minimization_faure.py
@@ -47,7 +47,7 @@ def getClosureGroup(tuple, table):
     elif (tuple[1] in reverse_conns):
     	minimal_tableau_pos = reverse_conns[tuple[1]] - 1
     else:
-    	return tuple # constants tuple like (1,1) only have themselves in the closure group
+    	return [tuple] # constants tuple like (1,1) only have themselves in the closure group
     minimal_tableau = closure_overhead.calculate_tableau(table, reverse_conns, len(conns))
     return minimal_tableau[minimal_tableau_pos]
 
@@ -65,7 +65,7 @@ def minimize(tablename = 't_v', pos = 0, summary = ['1','2']):
     Parameters:
     ------------
     tablename : string 
-        The name of table that needs to be minimized
+        The name of table stored in postgres that needs to be minimized.
     pos : int
         The current index of tuple that is being tested for removal. When calling this function, pos should be 0
     summary : list
@@ -78,6 +78,7 @@ def minimize(tablename = 't_v', pos = 0, summary = ['1','2']):
 
     conn = psycopg2.connect(host=host,user=user,password=password,database=database)
     cur = conn.cursor()
+
     # get current table
     curr_table = getCurrentTable(tablename, cur)
     conn.commit()
@@ -89,11 +90,9 @@ def minimize(tablename = 't_v', pos = 0, summary = ['1','2']):
     # get closure group of tuple in question
     tuple_to_remove = curr_table[pos]
     closure_group = getClosureGroup(tuple_to_remove, curr_table)
+    variables = closure_overhead.find_variables(curr_table)
     # print("tuple_to_remove: ", tuple_to_remove)
     # print("closure_group: ", closure_group)
-    if (closure_group == tuple_to_remove):
-    	return minimize(tablename, pos+1, summary)
-    	
 
     # get new table with removed tuple
     new_table_name = tablename+str(pos)
@@ -101,9 +100,10 @@ def minimize(tablename = 't_v', pos = 0, summary = ['1','2']):
     new_table.pop(pos)
     deleteTuple(new_table, new_table_name, cur)
 
-    # check for query containment
     sql_query = tableau.convert_tableau_to_sql(closure_group, new_table_name, summary)
-    # print("sql:",sql_query)
+    # print("sql:", sql_query)
+
+    # check for query containment
     tree = translator.generate_tree(sql_query)
     conn.commit()
     conn.close()
@@ -114,7 +114,7 @@ def minimize(tablename = 't_v', pos = 0, summary = ['1','2']):
 
     conn = psycopg2.connect(host=host,user=user,password=password,database=database)
     cur = conn.cursor()
-    if (check_tautology.table_contains_answer(output_table_name, summary)):
+    if (check_tautology.table_contains_answer(output_table_name, summary, variables)):
         # cur.execute("DROP TABLE IF EXISTS {}".format(output_table_name))
         cur.execute("DROP TABLE IF EXISTS {}".format(tablename))
         conn.commit()
@@ -133,12 +133,45 @@ if __name__ == '__main__':
     conn = psycopg2.connect(host=host,user=user,password=password,database=database)
     cursor = conn.cursor()
 
-    # Toy example of minimization
+    Toy example of minimization
     curr_type = "text"
-    tablename = "test_table"
+    tablename = "t_v"
     cursor.execute("DROP TABLE IF EXISTS {};".format(tablename))
     cursor.execute("CREATE TABLE {}(n1 {}, n2 {}, condition TEXT[]);".format(tablename, curr_type, curr_type))
-    conn.commit()
+    conn.commit()    
+
+    # curr_type = "text"
+    # tablename = "t_v"
+    # cursor.execute("DROP TABLE IF EXISTS {};".format(tablename))
+    # cursor.execute("CREATE TABLE {}(fid {}, n1 {}, n2 {}, condition TEXT[]);".format(tablename, "text", curr_type, curr_type))
+    # conn.commit()
+    # cursor.execute("INSERT INTO {} VALUES ('{}','{}', '{}', array[]::text[]);".format(tablename, 'f', '0', '1'))
+    # cursor.execute("INSERT INTO {} VALUES ('{}','{}', '{}', array[]::text[]);".format(tablename, 'f', '1', '1'))
+    # cursor.execute("INSERT INTO {} VALUES ('{}','{}', '{}', array[]::text[]);".format(tablename, 'f', '1', '2'))
+    # conn.commit()
+    # conn.close()
+    # exit()
+
+    # One big switch example
+    # cursor.execute("INSERT INTO {} VALUES ('{}', '{}', array[]::text[]);".format(tablename, '1', 'x'))
+    # cursor.execute("INSERT INTO {} VALUES ('{}', '{}', array[]::text[]);".format(tablename, 'x', 'y'))
+    # cursor.execute("INSERT INTO {} VALUES ('{}', '{}', array[]::text[]);".format(tablename, 'y', '2'))
+    # cursor.execute("INSERT INTO {} VALUES ('{}', '{}', array[]::text[]);".format(tablename, '1', 'x'))
+    # cursor.execute("INSERT INTO {} VALUES ('{}', '{}', array[]::text[]);".format(tablename, 'x', 'z'))
+    # cursor.execute("INSERT INTO {} VALUES ('{}', '{}', array[]::text[]);".format(tablename, 'z', 'y'))
+    # cursor.execute("INSERT INTO {} VALUES ('{}', '{}', array[]::text[]);".format(tablename, 'y', '2'))
+
+    # # New minimization example
+    # cursor.execute("INSERT INTO {} VALUES ('{}', '{}', array[]::text[]);".format(tablename, 'x', 'y'))
+    # cursor.execute("INSERT INTO {} VALUES ('{}', '{}', array[]::text[]);".format(tablename, 'y', 'y1'))
+    # cursor.execute("INSERT INTO {} VALUES ('{}', '{}', array[]::text[]);".format(tablename, 'y1', 'y2'))
+    # cursor.execute("INSERT INTO {} VALUES ('{}', '{}', array[]::text[]);".format(tablename, 'y2', 'y'))
+    # cursor.execute("INSERT INTO {} VALUES ('{}', '{}', array[]::text[]);".format(tablename, 'y', 'y'))
+    # cursor.execute("INSERT INTO {} VALUES ('{}', '{}', array[]::text[]);".format(tablename, 'y', 'z'))
+    # cursor.execute("INSERT INTO {} VALUES ('{}', '{}', array[]::text[]);".format(tablename, 'z', 'z2'))
+    # cursor.execute("INSERT INTO {} VALUES ('{}', '{}', array[]::text[]);".format(tablename, 'z', 'z'))
+
+    # Minimization toy example
     cursor.execute("INSERT INTO {} VALUES ('{}', '{}', array[]::text[]);".format(tablename, '1', '1'))
     cursor.execute("INSERT INTO {} VALUES ('{}', '{}', array[]::text[]);".format(tablename, 'y1', 'y1'))
     cursor.execute("INSERT INTO {} VALUES ('{}', '{}', array[]::text[]);".format(tablename, 'y2', 'y2'))
