0ee232009a31d55883d2766bc33d3728e4edc97e
lanfangping <34638594+lanfangping@users.noreply.github.com>
2022-04-01 12:18:34 -0400

    scripts for generate chain with loops

--- util/chain_generation/README.md
+++ util/chain_generation/README.md
@@ -1,6 +1,7 @@
 # Chain with loops
 
-`gen_chain_with_loop(size, rate_summary, size_single_loop)`:
+`gen_chain_with_loop(size, rate_summary, size_single_loop)`: Generate chain topology with loops. Configure size of chain topology, size of summary nodes and the number of variable nodes in a single loop.
+
 ## Input: 
 
 - size: integer
@@ -20,6 +21,21 @@
   - the list of variable nodes
 - picked_nodes: list
   - the list of constant nodes which connect to loops.
+
+`gen_chain_with_loop_control_position(size, rate_summary, rate_loops)`: Generate chain topology with loops. Configure size of chain topology, size of summary nodes and the number of summary nodes who are connected to loops.
+
+## Input: 
+
+- size: integer
+  - The number of nodes in the topology
+- rate_summary: float
+  - the percentage of nodes that appears in the summary; ranges [0, 1]
+- rate_loops:float
+  - the percentage of summay nodes that contains the loop; ranges [0, 1]
+  
+## output:
+
+Same as the above.
 
 # generate the corresponding tableau for chain with loops
 
--- util/chain_generation/gen_chain.py
+++ util/chain_generation/gen_chain.py
@@ -2,9 +2,13 @@
 Generate chain with loops
 """
 import math
+import random
 
 def gen_chain_with_loop(size=15, rate_summary=0.6, size_single_loop=4):   
     """
+    Generate chain topology with loops. 
+    Configure size of chain topology, size of summary nodes and the number of variable nodes in a single loop.
+
     Parameters:
     -------------
     size: integer
@@ -62,6 +66,63 @@ def gen_chain_with_loop(size=15, rate_summary=0.6, size_single_loop=4):
         i += 1
     print(path)
     return path, summary_nodes, variable_nodes, picked_nodes
+
+def gen_chain_with_loop_control_position(size=15, rate_summary=0.3, rate_loops=0.6):   
+    """
+    Generate chain topology with loops. 
+    Configure size of chain topology, size of summary nodes and the number of summary nodes who are connected to loops.
+
+    Parameters:
+    -------------
+    size: integer
+        The number of nodes in the topology
+    rate_summary: float
+        the percentage of nodes that appears in the summary; ranges [0, 1]
+    rate_loops:float
+        the percentage of summay nodes that contains the loop; ranges [0, 1]
+    
+    """
+    path = []
+    num_summary_nodes = math.ceil(size * rate_summary)
+    summary_nodes = ["{}".format(num+1) for num in range(num_summary_nodes)]
+    for i in range(num_summary_nodes-1):
+        path.append((summary_nodes[i], summary_nodes[i+1]))
+    print(path)
+
+    total_num_loops = math.ceil(num_summary_nodes * rate_loops)
+    avg_num_nodes_in_loop = math.ceil((size - num_summary_nodes) / total_num_loops)
+
+    picked_nodes = random.sample(summary_nodes, total_num_loops)
+    print(picked_nodes)
+    numbers = []
+    count = 0
+    for i in range(total_num_loops-1):
+        # num = random.randint(avg_num_nodes_in_loop-1, avg_num_nodes_in_loop+1)
+        num = avg_num_nodes_in_loop
+        count += num
+        numbers.append(num)
+    numbers.append((size - num_summary_nodes) - count)
+    print(numbers)
+    i = 1
+    loop_nodes = []
+    for idx, node in enumerate(picked_nodes):
+        if numbers[idx] == 1:
+            path.append((node, "y{}".format(i+j)))
+            path.append(("y{}".format(i+j), node))
+            continue
+        for j in range(numbers[idx]):
+            if j == 0:
+                path.append((node, "y{}".format(i+j)))
+                path.append(("y{}".format(i+j), "y{}".format(i+j+1)))
+            elif j == numbers[idx] - 1:
+                path.append(("y{}".format(i+j), node))
+            else:
+                path.append(("y{}".format(i+j), "y{}".format(i+j+1)))
+            loop_nodes.append("y{}".format(i+j))
+        # path.append(("y{}".format(i+j+1), node))
+        i += numbers[idx]
+    return path, summary_nodes, loop_nodes, picked_nodes
+
 
 def gen_tableau(path, picked_nodes):
     tuples = []
