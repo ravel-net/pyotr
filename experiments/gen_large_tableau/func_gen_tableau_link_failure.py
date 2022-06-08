3219e538c77f97054c738da505ce592aeb7be4ba
lanfangping <34638594+lanfangping@users.noreply.github.com>
2022-06-07 23:38:03 -0400

    replace hamiltonian path to shortest path

--- experiments/gen_large_tableau/func_gen_tableau_link_failure.py
+++ experiments/gen_large_tableau/func_gen_tableau_link_failure.py
@@ -6,6 +6,7 @@ print(root)
 sys.path.append(root)
 
 import psycopg2
+from psycopg2.extras import execute_values
 import databaseconfig as cfg
 import random
 import os
@@ -46,101 +47,45 @@ def load_topo(file_dir, out_tablename):
     conn.close()
     print("Done!")
 
-def add_backup_links_and_filters(dest, path_nodes, topo_tablename, forward_tablename, pick_num):
+def add_backup_links_and_filters(path_nodes, forward_tablename, pick_num):
     conn = psycopg2.connect(host=cfg.postgres['host'], database=cfg.postgres['db'], user=cfg.postgres['user'], password=cfg.postgres['password'])
     cursor = conn.cursor()
 
-    # get all nodes who have multiple neighbors in graph except root node
-    cursor.execute("select n1 from {} group by n1 having count(*) > 1".format(topo_tablename))
-    nodes = cursor.fetchall()
-    nodes = [node[0] for node in nodes]
-    # print(len(nodes))
-    if dest in nodes:
-        # nodes = set(nodes)
-        nodes.remove(dest)
+    if pick_num > len(path_nodes[:-2]):
+        print("invalid pick_num! Please decrease pick_num or rerun script")
+        exit()
 
-    # picked nodes should locate before the second last node
-    nodes1 = set(nodes)
-    nodes2 = set(path_nodes[:-2])
+    # randomly pick `pick_num` nodes to set backup links
+    picked_nodes = random.sample(path_nodes[:-2], pick_num)
 
-    suitable_nodes = nodes1.intersection(nodes2)
-
-    # random pick count nodes
-    count = pick_num
-    if len(suitable_nodes) < pick_num:
-        count = len(suitable_nodes)
-    picked_nodes = random.sample(list(suitable_nodes), k = count)
-
-    # store the variable of flag for backup links and its domain (in case there are multiple backup links for a primary link)
     flag_variables = {}
+    for picked_node in picked_nodes:
+        idx_picked_node = path_nodes.index(picked_node)
+        # print("pickde_node", picked_node)
+        # print("idx_picked_node", idx_picked_node)
 
-    linked_neighbor_cur  = conn.cursor()
-    check_cur = conn.cursor()
-    upd_cur = conn.cursor()
-    insert_cur = conn.cursor()
-    i = 0
-    while i < count:
-        node = random.sample(list(suitable_nodes), k = 1)[0]
-        
-        # get all neighbors
-        cursor.execute("select distinct n2 from {} where n1 = {}".format(topo_tablename, node))
-        neighbors = cursor.fetchall()
-        neighbors = set([row[0] for row in neighbors])
-        # print(neighbors)
-
-        # get linked neighbors
-        # node -> ?
-        linked_neighbor_cur.execute("select distinct n2 from {} where n1 = {}".format(forward_tablename, node)) 
-        linked_neighbors = linked_neighbor_cur.fetchall()
-        linked_neighbors = set([row[0] for row in linked_neighbors])
-
-        # get unlinked neighbors
-        unlinked_neighbors = neighbors - linked_neighbors
-        
-        if len(unlinked_neighbors) == 0:
-            continue
-
-        # the neighbor node should locate after this node
-        loc_node = path_nodes.index(node)
-        nodes3 = set(path_nodes[loc_node+1:])
-
-        allowed_nodes = unlinked_neighbors.intersection(nodes3)
-
-        if len(allowed_nodes) == 0:
-            continue
-
-        # if dest in unlinked_neighbors:
-        #     unlinked_neighbors.remove(dest)
-
-        # randomly pick one unlinked neighbor node
-        n = random.sample(list(allowed_nodes), k = 1)[0]
-        
         '''
-        # update condition of primary link (currently only one primary link from node to next node)
+        # update condition of primary link (currently only one primary link from `picked_node` to its next node)
         add ACL for picked nodes
         '''
-        check_cur.execute("select * from {} where n1 = {}".format(forward_tablename, node))
-        row = check_cur.fetchone()
-
         # store flag variable and its domain
-        flag_var = "l{}_{}".format(row[0], row[1])
+        flag_var = "l{}_{}".format(picked_node, path_nodes[idx_picked_node+1])
         domain_var = [0, 1]
         flag_variables[flag_var] = domain_var
 
         # condition and acl for primary link
         cond = "{} == {}".format(flag_var, 1)
-        upd_cur.execute("update {} set condition = array_append(condition, '{}') where n1 = {} and n2 = {}".format(forward_tablename, cond, row[0], row[1]))
-        upd_cur.execute("update {} set s = array_append(s, {}) where n1 = {} and n2 = {}".format(forward_tablename, node, row[0], row[1]))
+        cursor.execute("update {} set condition = array_append(condition, '{}') where n1 = {} and n2 = {}".format(forward_tablename, cond, picked_node, path_nodes[idx_picked_node+1]))
+        cursor.execute("update {} set s = array_append(s, {}) where n1 = {} and n2 = {}".format(forward_tablename, picked_node, picked_node, path_nodes[idx_picked_node+1]))
         conn.commit()
+
+        bp_next_node = random.sample(path_nodes[idx_picked_node+1:], 1)[0]
 
         # condition for backup link (one backup link for a primary link)
         bp_cond = "{} == {}".format(flag_var, 0)
-        insert_cur.execute("insert into {} values({}, {}, '{}', '{}')".format(forward_tablename, node, n, "{"+str(node)+"}", "{"+ bp_cond + "}"))
+        cursor.execute("insert into {} values({}, {}, '{}', '{}')".format(forward_tablename, picked_node, bp_next_node, "{"+str(picked_node)+"}", "{"+ bp_cond + "}"))
 
         conn.commit()
-
-        i += 1
-        suitable_nodes.remove(node) # remove picked node from waitlist
     conn.close()
 
 def load_tree_in_f(links, ftable_name):
@@ -159,10 +104,14 @@ def load_tree_in_f(links, ftable_name):
 
     cursor.executemany("insert into {} values(%s, %s, %s, %s)".format(ftable_name), tuples)
     conn.commit()
+    conn.close()
 
 def gen_hamiltonian_path(links):
     G = nx.DiGraph()
     G.add_edges_from(links)
+
+    is_dag = nx.is_directed_acyclic_graph(G)
+    print(is_dag)
 
     # calculate hamiltonian path
     path_nodes = tournament.hamiltonian_path(G)
@@ -174,6 +123,26 @@ def gen_hamiltonian_path(links):
         edges.append((path_nodes[i], path_nodes[i+1]))
 
     return edges, path_nodes, source, dest
+
+def gen_shortest_path(links):
+    G = nx.Graph()
+    G.add_edges_from(links)
+
+    nodes = G.nodes
+
+    two_nodes = random.sample(nodes, 2)
+
+    source = two_nodes[0]
+    dest = two_nodes[1]
+
+    path_nodes = nx.dijkstra_path(G, source, dest)
+
+    edges = []
+    for i in range(len(path_nodes)-1):
+        edges.append((path_nodes[i], path_nodes[i+1]))
+
+    return edges, path_nodes, source, dest
+
 
 def get_largest_connected_network(links):
     # print("original links", len(links))
@@ -211,6 +180,44 @@ def get_largest_connected_network(links):
     else:
         return links, list(G.nodes)
 
+def gen_connectivity_view(path_nodes, tablename, table_attributes, table_datatypes):
+    """
+    generate connectivity view for chase
+
+    Parameters:
+    ------------
+    path_nodes: list[integer/string]. The list of nodes in path. E.g., path 1->2->3. the path_nodes is [1, 2, 3].
+
+    tableaname: string. The name of database table that stores the connectivity view.
+
+    table_attributes: list[string]. The attributes of the database table <tablename>.
+
+    table_datatypes: list[string]. The datatype of each attribute of the databse table <tablename>.
+
+    """
+    tuples = []
+    tuples.append(('f', 's0', 'd0', 's', str(path_nodes[0]), '{}'))
+
+    for i in range(len(path_nodes)-1):
+        s = "s{}".format(i+1)
+        d = "d{}".format(i+1)
+
+        tuples.append(('f', s, d, str(path_nodes[i]), str(path_nodes[i+1]), '{}'))
+    tuples.append(('f', 's{}'.format(len(path_nodes)), 'd{}'.format(len(path_nodes)), str(path_nodes[-1]), 'd', '{}'))
+    conn = psycopg2.connect(host=cfg.postgres['host'], database=cfg.postgres['db'], user=cfg.postgres['user'], password=cfg.postgres['password'])
+    cursor = conn.cursor()
+
+    cursor.execute("Drop table if exists {}".format(tablename))
+
+    attributes = ["{} {}".format(table_attributes[i], table_datatypes[i]) for i in range(len(table_attributes))]
+    cursor.execute("create table {} ({})".format(tablename, ", ".join(attributes)))
+    execute_values(cursor, "insert into {} values %s".format(tablename), tuples)
+
+    conn.commit()
+    conn.close()
+    
+        
+
 if __name__ == '__main__':
     conn = psycopg2.connect(host=cfg.postgres['host'], database=cfg.postgres['db'], user=cfg.postgres['user'], password=cfg.postgres['password'])
     cursor = conn.cursor()
@@ -244,9 +251,13 @@ if __name__ == '__main__':
     print("hamiltonian path:", len(path_links))
 
     # load spanning tree into db (without backup and filters)
-    load_tree_in_f(path_links, fwd_tablename)
+    # load_tree_in_f(path_links, fwd_tablename)
 
     # conn.close()
     # add backup links to spanning tree
-    add_backup_links_and_filters(dest, path_nodes, topo_tablename, fwd_tablename, 2)
+    # add_backup_links_and_filters(dest, path_nodes, topo_tablename, fwd_tablename, 2)
+    tablename = "E"
+    table_attributes = ["f", 'src', 'dst', 'n', 'x', 'condition']
+    table_datatypes = ['int4_faure', 'int4_faure', 'int4_faure', 'int4_faure', 'int4_faure', 'text[]']
+    gen_connectivity_view(path_nodes, tablename, table_attributes, table_datatypes)
 
