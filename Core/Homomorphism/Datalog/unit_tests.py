23f0e9c7004a2d7eb90de906aa659d7ce6fb122f
mudbri <mubashiranwar@live.com>
2022-10-13 00:56:31 -0500

    Adding test for new conditions

--- Core/Homomorphism/Datalog/unit_tests.py
+++ Core/Homomorphism/Datalog/unit_tests.py
@@ -1,11 +1,10 @@
 import program as program
+import time
 
 if __name__ == "__main__":
     ################################# Example 6 - Containment ##################################
     p1 = "G(x,z) :- A(x,z)\nG(x,z) :- G(x,y),G(y,z)"
     p2 = "G(x,z) :- A(x,z)\nG(x,z) :- A(x,y),G(y,z)"
-    # print(p1)
-    # print(p2)
     program1 = program.DT_Program(p1)
     program2 = program.DT_Program(p2)
     if not program1.contains(program2):
@@ -13,22 +12,15 @@ if __name__ == "__main__":
         exit()
     else:
         print("Test 1.1 passed")
-
     if program2.contains(program1):
         print("Test 1.2 failed")
         exit()
     else:
         print("Test 1.2 passed")
 
-
     ################################ Example 7 - Minimization################################
     p1 = "G(x,y,z) :- G(x,w,z),A(w,y),A(w,z),A(z,z),A(z,y)"
-    # p2 = "G(x,y,z) :- G(x,w,z),A(w,z),A(z,z),A(z,y)"
     program1 = program.DT_Program(p1)
-    # program2 = DT_Program(p2)
-    # print(program1.contains(program2))
-    # print(program2.contains(program1))    
-    # print(program1)
     program1.minimize()
     if (str(program1) != "G(x,y,z) :- G(x,w,z),A(w,z),A(z,z),A(z,y)"):
         print("Test 2 failed")
@@ -38,11 +30,7 @@ if __name__ == "__main__":
 
     ########################################## ACL Example:  ######################################
     p1 = "R(a3, h3, [h3], 1)[h3 = 10] :- l(a3,h3)[h3 = 10], l(a3,e1)\nR(a2, h3, [h3], 1) :- l(a2,h3), l(a2, h4), l(a2, e1)\nR(e1, h3, [a2, x], 2) :- R(a2, h3, [x], 1), l(a2, e1), l(a1, e1), l(a3, e1)\nR(e1, h3, [a3, x], 2) :- R(a3, h3, [x], 1), l(a2, e1), l(a1, e1), l(a3, e1)\nR(a1, h3, [e1, x, y], 3)[h3 = 10] :- R(e1, h3, [x, y], 2)[h3 = 10], l(a1, h1), l(a1, e1), l(a1, h2)\nR(h1, h3, [a1, x, y, z], 4) :- R(a1, h3, [x, y, z], 3), l(a1, h1)\nR(h2, h3, [a1, x, y, z], 4) :- R(a1, h3, [x, y, z], 3), l(a1, h2)"
-
-    # p1 = "R(a3, h3, [h3], 1)[h3 = 10] :- l(a3,e1)\nR(a2, h3, [h3], 1)[h3 = 20]:- l(a2,h3)[{h3 = 20 and h3 = 30} or h3 = 40], l(a2, e1)"
-
     program1 = program.DT_Program(p1, {"R":["int4_faure", "int4_faure","int4_faure[]", "integer"], "l":["int4_faure", "int4_faure"]}, domains=['10', '20'], c_variables=['h3'], reasoning_engine='z3', reasoning_type='Int', datatype='int4_faure', simplification_on=True, c_tables=["R", "l"])
-
     program1.minimize()
     if (str(program1) != "R(a2,h3,[h3],1) :- l(a2,h3),l(a2,h4),l(a2,e1)\nR(e1,h3,[a3, x],2) :- R(a3,h3,[x],1),l(a3,e1)\nR(a1,h3,[e1, x, y],3)[h3 == 10] :- R(e1,h3,[x, y],2)[h3 == 10],l(a1,e1)\nR(h2,h3,[a1, x, y, z],4) :- R(a1,h3,[x, y, z],3),l(a1,h2)"):
         print("Test 3 failed")
@@ -54,7 +42,6 @@ if __name__ == "__main__":
     # Tests is_head_contained_faure
     p1 = "R(x, y) :- l(x,y)\nR(x,z) :- R(x,y), l(y,z)"
     program1 = program.DT_Program(p1, {"R":["int4_faure", "int4_faure"], "l":["int4_faure", "int4_faure"]}, domains=['1', '2'], c_variables=['x','y','z'], reasoning_engine='z3', reasoning_type='Int', datatype='int4_faure', simplification_on=True, c_tables=["R", "l"])
-
     program1.minimize()
     if (str(program1) != "R(x,y) :- l(x,y)\nR(x,z) :- R(x,y),l(y,z)"):
         print("Test 4 failed")
@@ -62,18 +49,28 @@ if __name__ == "__main__":
     else:
         print("Test 4 passed")
 
-
     ########################################## c-variable as header test  ######################################
+    # Tests c-variable appearing in header that does not appear anywhere in the body
     p1 = "R(x,y) :- L(x,q,z), Q(z)\nR(x,y) :- L(x,q,z), Q(z)"
-
     program1 = program.DT_Program(p1, {"R":["integer", "int4_faure"], "L":["integer", "integer", "int4_faure"], "Q":["int4_faure"]}, domains=['1', '2'], c_variables=['z','y'], reasoning_engine='z3', reasoning_type='Int', datatype='int4_faure', simplification_on=True, c_tables=["R", "L", "Q"])
-
     program1.minimize()
     if (str(program1) != "R(x,y) :- L(x,q,z),Q(z)"):
         print("Test 5 failed")
         exit()
     else:
         print("Test 5 passed")
+
+    ########################################## New Condition Format Test  ######################################
+    # Note that the brackets in the conditions are curly brackets. TODO: Fix constraint parser so that they don't have to be square brackets
+    # Also note that the header conditions are represented in z3 format while the body conditions are represented in sql format. TODO: Write a Z3 to SQL parser
+    p1 = "R(x,y)[And{y = 10, y < 20}] :- L(x,y,z)[y = 10 and y < 20], Q(z)\nR(x,y) :- L(x,q,z), Q(z)"
+    program1 = program.DT_Program(p1, {"R":["integer", "int4_faure"], "L":["integer", "int4_faure", "int4_faure"], "Q":["int4_faure"]}, domains=['1', '2'], c_variables=['z','y'], reasoning_engine='z3', reasoning_type='Int', datatype='int4_faure', simplification_on=True, c_tables=["R", "L", "Q"])
+    program1.minimize()
+    if (str(program1) != "R(x,y)[And(y == 10,  y < 20)] :- L(x,y,z)[y == 10 and y < 20],Q(z)\nR(x,y) :- L(x,q,z),Q(z)"):
+        print("Test 6 failed")
+        exit()
+    else:
+        print("Test 6 passed")
     # =============================== Route Agg
 #     p2 = "R(v,d)[d > 10] :- R(u,d)[d > 10], l(v,u)"
 # # 
