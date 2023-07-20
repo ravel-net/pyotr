ff5115b38c6684fe75c27b6949aae7eafa620052
mudbri <mubashiranwar@live.com>
2023-07-19 21:43:05 -0500

    Breakdown arrays

--- Core/Datalog/conditionTree.py
+++ Core/Datalog/conditionTree.py
@@ -13,105 +13,25 @@ class ConditionLeaf:
     A class used to represent a single condition of the form 'var1 op var2'
     """
     # Leafs are conditions of the form var1 op var2
-    @timeit
-    def __init__(self, condition, pos):
-        self.endPos = parsing_utils.findCondEnd(condition, pos)
-        self.operator = parsing_utils.findOperator(condition, pos, self.endPos)
-        currCond = deepcopy(condition[pos:self.endPos])
+    def __init__(self, currCond, operator):
+        self.operator = operator
         conditionSplit = currCond.split(self.operator)
         self.var1 = conditionSplit[0].strip()
         self.var2 = conditionSplit[1].strip()
-        if self.endPos < len(condition) and condition[self.endPos] == ",":
-            self.endPos += 1
-    
-    # @property
-    def getLeafEndPos(self):
-        return self.endPos
-    
+
     def __str__(self):
-        return self.var1 + " " + self.operator + " " + self.var2
+        return parsing_utils.condToStringDefault(self.var1, self.operator, self.var2)
 
-    def replacedString(self, replacementDict):
-        newVar1 = self.var1
-        newVar2 = self.var2
-        if self.var1 in replacementDict:
-            newVar1 = replacementDict[self.var1]
-        if self.var2 in replacementDict:
-            newVar2 = replacementDict[self.var2]
-        return newVar1 + " " + self.operator + " " + newVar2
-    
-    def sqlString(self, atomTables):
-        newOp = self.operator
-        newVar1 = self.var1
-        newVar2 = self.var2        
-        if self.operator == "==":
-            newOp = "="
-
-        if not self.var1[0].isdigit():
-            var1_table = self.var1.split(".")[0]
-            var1_colm = self.var1.split(".")[1]
-            var1_type = atomTables[var1_table].columns[var1_colm]
-            if "[]" in var1_type and self.operator == "!=":
-                newVar1 = "All(" + self.var1 + ")"
-            elif "[]" in var1_type:
-                newVar1 = "Any(" + self.var1 + ")"
-        if not self.var2[0].isdigit():
-            var2_table = self.var2.split(".")[0]
-            var2_colm = self.var2.split(".")[1]
-            var2_type = atomTables[var2_table].columns[var2_colm]
-            if "[]" in var2_type and self.operator == "!=":
-                newVar2 = "All(" + self.var2 + ")"
-            elif "[]" in var2_type:
-                newVar2 = "Any(" + self.var2 + ")"
-
-        return newVar1 + " " + newOp + " " + newVar2
-    
-    def arrayIntegrationString(self, atomTables):
-        newOp = self.operator
-        newVar1 = self.var1
-        newVar2 = self.var2        
-        if self.operator == "=":
-            newOp = "=="
-        if not self.var1[0].isdigit():
-            var1_table = self.var1.split(".")[0]
-            var1_colm = self.var1.split(".")[1]
-            var1_type = atomTables[var1_table].columns[var1_colm]
-            if "[]" in var1_type:
-                newVar1 = self.var1 + "::text"
-        if not self.var2[0].isdigit():
-            var2_table = self.var2.split(".")[0]
-            var2_colm = self.var2.split(".")[1]
-            var2_type = atomTables[var2_table].columns[var2_colm]
-            if "[]" in var2_type:
-                newVar2 = self.var2 + "::text"
-        return newVar1 + " " + newOp + " " + newVar2
-
-    def negativeIntString(self, atomTables):
-        conditions = []
-        conditions.append(str(self)) # first condition is the default one
-        if not self.var1[0].isdigit():
-            var1_table = self.var1.split(".")[0]
-            var1_colm = self.var1.split(".")[1]
-            var1_type = atomTables[var1_table].columns[var1_colm]
-            if "faure" in var1_type: # c-table
-                newCondition = parsing_utils.negativeIntCondition(self.var1, var1_type)
-                conditions.append(newCondition)
-        if not self.var2[0].isdigit():
-            var2_table = self.var2.split(".")[0]
-            var2_colm = self.var2.split(".")[1]
-            var2_type = atomTables[var2_table].columns[var2_colm]
-            if "faure" in var2_type: # c-table
-                newCondition = parsing_utils.negativeIntCondition(self.var2, var2_type)
-                conditions.append(newCondition)
-        return "Or(" + ", ".join(conditions) + ")"
+    def toString(self, mode, replacementDict = {}, atomTables = [], reasoningType={}):
+        return parsing_utils.condToStringModes(var1=self.var1, operator=self.operator, var2=self.var2, mode=mode, replacementDict=replacementDict, atomTables=atomTables, reasoningType={})
 
 # make sure to check if the conditionTree isTrue or not. If it is, then it is trivially true and can be skipped altogether
 # ConditionTree is either a single condition or a logical operator with conditionTrees as children
+# Note that we break down arrays into elements e.g 'v != [1,2,3]' becomes 'And(v != 1, v != 2, v != 3)'
 class ConditionTree:
     """
     A class used to represent a condition using a tree. Each node is either a logical operator or a leaf node (that represents a single condition)
     """
-    @timeit
     def __init__(self, condition, pos = 0):
         self.condition_string = condition
         self.children = []
@@ -122,15 +42,10 @@ class ConditionTree:
         while pos < len(condition) and (condition[pos] == " " or condition[pos] == ","):
             pos += 1
         
-        isLogicalOP = True
         if len(condition) == 0:
             self.isEmpty = True
-            isLogicalOP = False
         elif len(condition)-pos < 4: # must be a leaf
-            self.value = ConditionLeaf(condition, pos)
-            self.endPos = self.value.getLeafEndPos()
-            self.isLeaf = True
-            isLogicalOP = False
+            self.processLeaf(condition=condition, pos=pos)
         elif condition[pos:pos+2] == "Or":
             while pos < len(condition) and condition[pos] != "(":
                 pos += 1
@@ -141,18 +56,20 @@ class ConditionTree:
                 pos += 1
             pos += 1
             self.value = "And"
+        elif condition[pos:pos+3] == "Not":
+            while pos < len(condition) and condition[pos] != "(":
+                pos += 1
+            pos += 1
+            self.value = "Not"
         else: # must be a leaf
-            self.value = ConditionLeaf(condition, pos)
-            self.endPos = self.value.getLeafEndPos()
-            self.isLeaf = True
-            isLogicalOP = False
+            self.processLeaf(condition=condition, pos=pos)
 
         # skip over spaces:
         while pos < len(condition) and condition[pos] == " ":
             pos += 1
 
         # The bottom part only run if it's a logical operator
-        if isLogicalOP:
+        if not self.isEmpty and not self.isLeaf:
             while pos < len(condition) and condition[pos] != ")":
                 child = ConditionTree(condition, pos)
                 # if not child.getIsTrue(): # when the condition is trivially true. #TODO: Also think about doing something when the condition is trivially False.
@@ -166,6 +83,30 @@ class ConditionTree:
 
             pos += 1 # skipping over last bracket
             self.endPos = pos
+
+    def processLeaf(self, condition, pos):
+        endPos = parsing_utils.findCondEnd(condition, pos)
+        operator = parsing_utils.findOperator(condition, pos, endPos)
+        currCond = deepcopy(condition[pos:endPos])
+        self.endPos = endPos
+        if "{" in currCond: # it is an array
+            self.isLeaf = False
+            if operator == "!=": # not in
+                self.value = "And"
+            else:
+                self.value = "Or"
+            conditionSplit = currCond.split(self.operator)
+            var1 = conditionSplit[0].strip()
+            var2Arr = conditionSplit[1].strip()[1:-1].split(",") # we assume that var2 is an array
+            for var2 in var2Arr:
+                var2 = var2.stript()
+                conditionLeaf = "{} {} {}".format(var1, operator, var2)
+                self.children.append(ConditionLeaf(currCond=conditionLeaf, operator=operator))
+        else:
+            self.isLeaf = True
+            self.value = ConditionLeaf(currCond, operator)
+        if self.endPos < len(condition) and condition[self.endPos] == ",":
+            self.endPos += 1
 
     # @property
     def getEndPos(self):
@@ -185,69 +126,20 @@ class ConditionTree:
             else:
                 return ""
     
-    # returns another condition tree but this time with added constraints for negative integers
-    def getNegativeIntegerTree(self, atomTables):
-        negativeIntString = self.negativeIntString(atomTables)
-        return ConditionTree(negativeIntString)
-
-    # returns current condition string but this time with added constraints for negative integers
-    def negativeIntString(self, atomTables):
+    def toString(self, mode, replacementDict={}, atomTables=[], reasoningType={}):
         if self.isEmpty:
             return ""
         if self.isLeaf:
-            return self.value.negativeIntString(atomTables)
+            return self.value.toString(mode = mode, atomTables=atomTables, replacementDict=replacementDict, reasoningType=reasoningType)
         else:
             childrenStr = []
             for child in self.children:
-                childrenStr.append(child.negativeIntString(atomTables))
+                childrenStr.append(child.toString(mode = mode, atomTables=atomTables, replacementDict=replacementDict, reasoningType=reasoningType))
             if len(childrenStr) > 0:
-                return self.value + "(" + ", ".join(childrenStr) + ")" 
-            else:
-                return ""   
-
-    # prints the condition but with variables replaced from replacementDictionary
-    def replacedString(self, replacementDict):
-        if self.isEmpty:
-            return ""
-        if self.isLeaf:
-            return self.value.replacedString(replacementDict)
-        else:
-            childrenStr = []
-            for child in self.children:
-                childrenStr.append(child.replacedString(replacementDict))
-            if len(childrenStr) > 0:
-                return self.value + "(" + ", ".join(childrenStr) + ")" 
-            else:
-                return ""     
-
-    # returns current condition string but this time in array integration format (::text with array variables and == instead of =)
-    def arrayIntegrationString(self, atomTables):
-        if self.isEmpty:
-            return ""
-        if self.isLeaf:
-            return self.value.arrayIntegrationString(atomTables)
-        else:
-            childrenStr = []
-            for child in self.children:
-                childrenStr.append(child.arrayIntegrationString(atomTables))
-            if len(childrenStr) > 0:
-                return self.value + "(" + ", ".join(childrenStr) + ")" 
-            else:
-                return ""
-
-    # prints an sql version of the conditionTree
-    def sqlString(self, atomTables):
-        if self.isEmpty:
-            return ""
-        if self.isLeaf:
-            return self.value.sqlString(atomTables)
-        else:
-            childrenStr = []
-            for child in self.children:
-                childrenStr.append(child.sqlString(atomTables))
-            # return "(" + " {} ".format(self.value).join(childrenStr) + ")"
-            if len(childrenStr) > 0:
-                return "(" + " {} ".format(self.value).join(childrenStr) + ")"   
+                if mode == "SQL":
+                    return "(" + " {} ".format(self.value).join(childrenStr) + ")"   
+                else:
+                    return self.value + "(" + ", ".join(childrenStr) + ")" 
             else:
                 return ""
 
